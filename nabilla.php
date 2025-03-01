c7c46b4a174a319dfd0a45770273096476eac663
Bileyyy <183796742+Bileyyy@users.noreply.github.com>
2025-03-01 13:00:53 +0700

    php bila

---
+++ nabilla.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>
+        php bila
+    </title>
+</head>
+<body>
+
+<?php
+    $walkes = "bu choir";
+    $kelas = "XI PPLG 3";
+
+    echo"<p>wali kelas = $walkes</p>";
+    echo $kelas."territtory";
+    echo"<p>selalu kompak ya".$kelas."sigma</p>";
+?> 
+
+</body>
+</html>
+
